2933729ffeb88fd79fa7a48ace2d05ae682a2700
chaitanya.shah <chaitanya.shah@YI1011368LT>
2021-05-03 11:32:36 +0530

    view change

---
+++ FitnesspointDatabase/PaymentTbl.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[PaymentTbl](
+[Order_id] [int] IDENTITY(1001,1) NOT NULL,
+[User_id] [int] NULL,
+[Plan_id] [int] NULL,
+[Plan_name] [varchar](50) NULL,
+[Amount] [int] NOT NULL,
+CONSTRAINT [PK_PaymentTbl] PRIMARY KEY CLUSTERED
+(
+[Order_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[PaymentTbl] WITH CHECK ADD CONSTRAINT [FK_PaymentTbl_NutritionPlan] FOREIGN KEY([Plan_id])
+REFERENCES [dbo].[NutritionPlan] ([NutriPlanId])
+GO
+ALTER TABLE [dbo].[PaymentTbl] CHECK CONSTRAINT [FK_PaymentTbl_NutritionPlan]
+GO
+ALTER TABLE [dbo].[PaymentTbl] WITH CHECK ADD CONSTRAINT [FK_PaymentTbl_UserDetails] FOREIGN KEY([User_id])
+REFERENCES [dbo].[UserDetails] ([UserId])
+GO
+ALTER TABLE [dbo].[PaymentTbl] CHECK CONSTRAINT [FK_PaymentTbl_UserDetails]
+GO
