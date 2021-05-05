7bc66e4294c8bd64767fe2671a6842503a3c4a63
nimish.vishnoi <nimish.vishnoi@YI1011380LT>
2021-05-05 15:31:28 +0530

    Testing

---
+++ Fitnesspoint.unittest/ControllerTest/AccountControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitnesspoint.Controllers;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Fitnesspoint.unittest.ControllerTest
+{
+    [TestClass]
+  public  class AccountControllerTest
+    {
+       
+            [TestMethod]
+            public void Login_Test()
+            {
+                //Arrange
+                AccountController ac = new AccountController();
+
+                //Act
+                ViewResult result = ac.Login() as ViewResult;
+
+                //Assert
+                Assert.IsNotNull(result);
+            }
+
+            [TestMethod]
+            public void Register_Test()
+            {
+                // Arrange
+                AccountController ac1 = new AccountController();
+
+                // Act
+                ViewResult result = ac1.Register() as ViewResult;
+
+                // Assert
+                Assert.IsNotNull(result);
+            }
+
+        }
+
+    }
+
+
---
+++ Fitnesspoint.unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fitnesspoint.unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fitnesspoint.unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0429816e-daff-45ec-a7c5-a77d03d0644f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
